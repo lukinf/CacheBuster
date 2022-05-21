0b7a21ea6ced5db56c58182df9cd48d7ff9651fa
Lukas Fridl <lukas@fridl.cz>
2022-05-21 11:57:24 +0200

    First run

--- cacheBuster/main.cpp
+++ cacheBuster/main.cpp
@@ -3,7 +3,7 @@
 //  cacheBuster
 //
 //  Created by Lukáš Frídl on 18.05.2022.
-//	/Users/lukas/Desktop/Projects/www.lukas.fridl.cz/;
+//	/Users/lukas/Desktop/Projects/www.lukas.fridl.cz/
 
 #include <iostream>
 #include "buster.hpp"
@@ -12,11 +12,22 @@
 
 int main(int argc, const char * argv[]) {
 	std::string path;
+	bool firstRun = false;
+	std::string firstRunS;
+	std::cout << "First run? Y/N:";
+	std::cin >> firstRunS;
 	std::cout << "Enter absolute path:";
 	std::cin >> path;
+	if (firstRunS == "Y") {
+		firstRun = true;
+	} else if (firstRunS == "N") {
+		firstRun = false;
+	} else {
+		return 0;
+	}
 	Directory directory = Directory(path);
 	Buster::test_run(directory.get_files());
-	Buster::run(false, directory.get_files());
+	Buster::run(firstRun, directory.get_files());
 	std::cout << "Done..." << std::endl;
 	return 0;
 }
