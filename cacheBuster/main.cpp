14533ff97eb95074f4c2fe949391b858b21e786c
Lukas Fridl <lukas@fridl.cz>
2022-05-21 13:31:49 +0200

    App Logic

--- cacheBuster/main.cpp
+++ cacheBuster/main.cpp
@@ -13,21 +13,43 @@
 int main(int argc, const char * argv[]) {
 	std::string path;
 	bool firstRun = false;
-	std::string firstRunS;
+	std::string qa;
+	
+first_run:
+	qa.clear();
 	std::cout << "First run? Y/N:";
-	std::cin >> firstRunS;
+	std::cin >> qa;
+	if (qa == "Y") {
+		qa = true;
+	} else if (qa == "N") {
+		qa = false;
+	} else {
+		goto first_run;
+	}
+
+enter_path:
 	std::cout << "Enter absolute path:";
 	std::cin >> path;
-	if (firstRunS == "Y") {
-		firstRun = true;
-	} else if (firstRunS == "N") {
-		firstRun = false;
-	} else {
-		return 0;
+	if (path.empty() == true){
+		goto enter_path;
 	}
+	
 	Directory directory = Directory(path);
 	Buster::test_run(directory.get_files());
-	Buster::run(firstRun, directory.get_files());
-	std::cout << "Done..." << std::endl;
+	std::cout << std::endl;
+	
+write_changes:
+	qa.clear();
+	std::cout << "Test run done, write changes? Y/N:";
+	std::cin >> qa;
+	if (qa == "Y") {
+		Buster::run(firstRun, directory.get_files());
+	} else if (qa == "N") {
+		std::cout << "No changes has been written..." << std::endl;
+	} else {
+		goto write_changes;
+	}
+	
+	std::cout << "Buster run done..." << std::endl;
 	return 0;
 }
